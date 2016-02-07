eab712e22bbdc9fd683e62f2ea22e74170951278
Dan Esposito <esposito.dan@gmail.com>
2016-02-07 17:29:35 -0500

    Home page and test of homepage.

---
+++ spec/features/home_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+feature "visiting the homepage" do
+	scenario "the visitor sees the company name" do
+		visit root_path
+		expect(page).to have_text("Better than AirBnB")
+	end
+end
+	
